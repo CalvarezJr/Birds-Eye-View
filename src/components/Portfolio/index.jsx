1ec6f05e9caff143ac0526349dc16ad0aedef340
Nick Bicknell <nicholasbicknell@gmail.com>
2024-12-10 13:12:58 -0600

    added portfolio logic

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import Project from "../Project";
+
 function Portfolio() {
+    const [projects] = useState([
+        {
+          desc: "E-commerce, React, MongoDB, GraphQl",
+          vid: "./projects/first-home.mp4"
+        },
+      ]);
     
+      return (
+        <div>
+          <div className="row justify-content-between">
+            <h1 className="display-2 text-white text-center mb-5">Portfolio</h1>
+          </div>
+          <div className="container d-flex align-items-center justify-content-center position-relative flex-wrap">
+            {projects.map((project, idx) => (
+              <Project project={project} key={"project" + idx} />
+            ))}
+          </div>
+        </div>
+      //    <Carousel>
+      //    <Carousel.Item>
+      //      <ExampleCarouselImage text="First slide" />
+      //      <Carousel.Caption>
+      //        <h3>First slide label</h3>
+      //        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      //      </Carousel.Caption>
+      //    </Carousel.Item>
+      //  </Carousel>
+      );
 }
 
 export default Portfolio;
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,5 +1,36 @@
 function Project() {
-    
+    const { desc, vid } = project;
+
+  return (
+    // <div className="card d-flex position-relative flex-column animate__animated animate__bounceInDown">
+    //   <a href={link}>
+    //   <div className="imgContainer">
+    //     <img src={img} />
+    //   </div>
+    //   </a>
+    //   <div className="content">
+    //       <h2 className="project-text">{name}</h2>
+    //     <a href={repo}>
+    //       <img
+    //         className="project-icon mx-2 my-2"
+    //         src="./img/github-icon.png"
+    //       />
+    //     </a>
+    //     <p className="project-text">{desc}</p>
+    //   </div>
+    // </div>
+    <div className="relative overflow-hidden animate__animated animate__bounceInDown">
+    <video
+      className="w-full h-full object-cover"
+      autoPlay
+      loop
+      muted
+    >
+      <source src={vid} type="video/mp4" />
+    </video>
+    <p>{desc}</p>
+    </div>
+  );
 }
 
 export default Project;
